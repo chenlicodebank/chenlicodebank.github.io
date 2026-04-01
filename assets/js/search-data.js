832b9270b4a0ace6d8fd9ed9633755cd7042155b
chenlicodebank <46190962+chenlicodebank@users.noreply.github.com>
2026-04-01 05:36:18 +0000

    Deploying to gh-pages from @ chenlicodebank/chenlicodebank.github.io@b39f621212496f7e6a6406675a31d0a8af247490 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Research projects on noise-resilient full-stack neuromorphic AI.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
